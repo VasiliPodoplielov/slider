3e092cb95228da8519a10c00303bafe063a3fdab
Vasyl Podoplielov <vasili.podoplelov@gmail.com>
2023-05-31 10:08:40 +0200

    Setuped eslint and pre-commit hook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 class Slider {
-    constructor(selector:string, options: Record<string, string>) {
-    }
+	constructor(selector: string, options: Record<string, string>) {
+		console.log('test');
+	}
 }
 
 console.log('adasdas');
